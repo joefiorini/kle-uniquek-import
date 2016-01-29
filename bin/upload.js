cbde8709db89d949fe26c340a15a921bafe32ad8
Joe Fiorini <joe@joefiorini.com>
2016-01-29 10:36:37 -0500

    Fix upload promise

--- bin/upload.js
+++ bin/upload.js
@@ -9,25 +9,6 @@ program
   .option('-b, --baud-rate [baudRate]', 'The baud rate for the serial port (defaults to 19200 for animus firmware)', parseInt, 19200)
   .parse(process.argv);
 
-function runCommand(command, rest, done) {
-  console.dir(command)
-  if (command === undefined) {
-    done();
-    return;
-  }
-
-  console.log(`Writing command: ${command}`);
-
-  serialPort.write(`${command} `, (err, results) => {
-    console.log('err: ', err);
-    console.log('results: ', results);
-  });
-
-  const [next, ...remaining] = rest;
-
-  console.log('Pausing between commands...');
-  setTimeout(() => runCommand(next, remaining, done), 50);
-}
 
 const {device, baudRate} = program;
 
@@ -43,19 +24,40 @@ new Promise((resolve, reject) => {
   .then(commands => {
     const serialPort = new SerialPort(device, {baudRate});
 
+    function runCommand(command, rest, done) {
+      if (command === undefined) {
+        done();
+        return;
+      }
+
+      console.log(`Writing command: ${command}`);
+
+      if (command !== '') {
+        serialPort.write(`${command} `, (err, results) => {
+          console.log('err: ', err);
+          console.log('results: ', results);
+        });
+      }
+
+      const [next, ...remaining] = rest;
+
+      console.log('Pausing between commands...');
+      setTimeout(() => runCommand(next, remaining, done), 50);
+    }
+
     console.log(`Connecting to ${device} at ${baudRate}`);
 
-    serialPort.on('open', () => {
-      serialPort.on('data', data => {
-        console.log('data received: ', data.toString('utf-8'));
-      });
+    return new Promise((resolve, reject) => {
+      serialPort.on('open', () => {
+        serialPort.on('data', data => {
+          console.log('data received: ', data.toString('utf-8'));
+        });
 
-      return new Promise((resolve, reject) => {
-        const [firstCommand,...rest] = commands;
-        runCommand(firstCommand, rest, resolve);
-      })
-      .then(() => serialPort.close());
-    });
+          const [firstCommand,...rest] = commands;
+          runCommand(firstCommand, rest, resolve);
+      });
+    })
+    .then(() => serialPort.close());
   })
   .catch(error => {
     console.error(error.message);
