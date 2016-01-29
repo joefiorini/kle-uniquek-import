10ca2e4c69334a924352eb5b4df72c334436b5a5
Joe Fiorini <joe@joefiorini.com>
2016-01-29 08:39:18 -0500

    Map keys to set key commands

--- bin/import-kle.js
+++ bin/import-kle.js
@@ -1,22 +1,37 @@
 import program from 'commander';
 import downloadData from '../lib/downloadData';
 import deserializeData from '../lib/deserializeData';
+import buildSetKeyCommands from '../lib/divergeUtils';
+import {flatten} from 'lodash/array';
+import fs from 'fs';
+
 const {version} = require('../package.json');
+
+function jsonFile(path) {
+  const file = fs.readFileSync(path, {encoding: 'utf-8'});
+  return JSON.parse(file);
+}
 
 program
   .version(version)
   .option('-d, --gist-id [gistId]', 'Data URL')
+  .option('-m, --custom-mappings [mappingFile]', 'JSON file containing custom keycode mappings', jsonFile)
+  .option('-l, --layer [layer]', 'The layer this layout corresponds to', parseInt, 0)
   .parse(process.argv)
   .action(env => {
   });
 
+const {gistId, layer, customMappings} = program;
+
 console.log('Downloading');
-downloadData(program.gistId)
+downloadData(gistId)
   .then(data => {
     console.log('Keyboard: ', data[0].name)
     return data;
   })
   .then(([,...rows]) => deserializeData(rows))
+  .then(flatten)
+  .then(keys => buildSetKeyCommands(keys, layer, customMappings))
   .then(result => console.log(result))
   .catch(error => {
     console.error(error.message);
